31671bf27fcbdc4c90fcb44d8812b8d1b5b7a015
MichielNoback <m.a.noback@pl.hanze.nl>
2016-01-10 20:41:49 +0100

    complex datatypes lesson in progress

---
+++ Complex_Datatypes/initLesson.R
@@ -0,0 +1,9 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+my.data <- c(2, 4, 3, 8, 5, 7, 6, 10, 6, 11, 7, 13)
+
+student.performance <- c("good", "poor", "poor", "good", "excellent", "good", "poor", "excellent", "excellent", "excellent", "good", "excellent", "poor", "good", "good", "good", "poor", "excellent", "good")
+
