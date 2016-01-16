fbe3ab1b7d98917ba1a53b3c7798727e324daa46
MichielNoback <m.a.noback@pl.hanze.nl>
2016-01-16 14:32:28 +0100

    complex datatypes lesson finished

--- Complex_Datatypes/initLesson.R
+++ Complex_Datatypes/initLesson.R
@@ -10,3 +10,14 @@ my.data <- c(2, 4, 3, 8, 5, 7, 6, 10, 6, 11, 7, 13)
 
 student.performance <- c("good", "poor", "poor", "good", "excellent", "good", "poor", "excellent", "excellent", "excellent", "good", "excellent", "poor", "good", "good", "good", "poor", "excellent", "good")
 
+my.courses <- c("Calculus", "Genetics 2", "Biochemistry", "Labwork", "Statistics")
+my.grades <- c(3.4, 6.8, 5.6, 8.5, 2.4)
+
+my.teachers <- c("Dr. Stein Franken", "Prof. Rose Watson-Crick", "Prof. Mendelejev", "Dr. Nobody", "Prof. Student")
+my.curriculum <- data.frame(course = my.courses, grade = my.grades, teacher = my.teachers)
+
+my.honours.curriculum <- data.frame(
+    course = c("Design Patterns", "Scientific Writing"),
+    grade = c(8.3, 5.6),
+    teacher = c("Dr Michail Noobrain", "Martin Herbman, MSc"),
+    attendance = c("always", "sometimes"))
